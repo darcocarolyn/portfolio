325ea9c66635ba00e51075063954f76c22017b6c
Carolyn <darcocarolyn@gmail.com>
2024-02-21 13:18:21 -0500

    commit

--- js/index.js
+++ js/index.js
@@ -2,3 +2,24 @@ console.log('Hello World!')
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+function sendEmail(){
+    let params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    }
+
+    const serviceID = "service_xeynjxd"
+    const templateID = "template_mq4h247"
+    
+    emailjs.send(serviceID, templateID, params)
+    .then(
+        res => {
+            document.getElementById("name").value,
+            document.getElementById("email").value,
+            document.getElementById("message").value  
+            console.log(res)
+            alert("Your message was sent successfully!")  
+        })
+        .catch((err) => console.log(err))
+}
